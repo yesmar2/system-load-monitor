f2d6cc36e06bce87b7a26b847aa8b5b3e9990504
unknown <ramsey.bland02@gmail.com>
2018-04-29 15:56:44 -0400

    Updated x grid to use time since

--- client/src/Chart.js
+++ client/src/Chart.js
@@ -18,6 +18,33 @@ var formatMinutes = function(d) {
     return output;
 };
 
+function timeSince(date) {
+    var minutes = (new Date() - date) / 1000 / 60;
+
+    //var interval = Math.floor(seconds / 31536000);
+
+    //   if (interval > 1) {
+    //     return interval + " years";
+    //   }
+    //   interval = Math.floor(seconds / 2592000);
+    //   if (interval > 1) {
+    //     return interval + " months";
+    //   }
+    //   interval = Math.floor(seconds / 86400);
+    //   if (interval > 1) {
+    //     return interval + " days";
+    //   }
+    //   interval = Math.floor(seconds / 3600);
+    //   if (interval > 1) {
+    //     return interval + " hours";
+    //   }
+    //   interval = Math.floor(seconds / 60);
+    //   if (interval > 1) {
+    //     return interval + " minutes";
+    //   }
+    return minutes;
+}
+
 class Chart extends React.Component {
     constructor(props) {
         super(props);
@@ -31,10 +58,10 @@ class Chart extends React.Component {
         const height = 500 - margin.top - margin.bottom;
 
         // set the ranges
-        const x = d3.scaleTime().range([0, width]);
+        const x = d3.scaleLinear().range([0, width]);
         const y = d3.scaleLinear().range([height, 0]);
 
-        const xAxis = d3.axisBottom(x);
+        const xAxis = d3.axisBottom(x).ticks(5, "s");
         const yAxis = d3.axisLeft(y);
 
         this.state = { x, y, xAxis, yAxis, margin, width, height };
@@ -51,32 +78,6 @@ class Chart extends React.Component {
         }
     }
 
-    componentWillReceiveProps(nextProps) {
-        //console.log(nextProps);
-        //this.updateStateFromProps(nextProps);
-        //this.renderD3();
-    }
-
-    // componentWillMount() {
-    //     const { xAxis, yAxis, grid } = this.state;
-
-    // const svg = d3.select(this.svg).transition();
-    // svg.select(`.${classes.xAxis}`)
-    //   .duration(100)
-    //   .call(xAxis);
-
-    //     svg
-    //         .append("g")
-    //         .attr("transform", "translate(0," + height + ")")
-    //         .call(d3.axisBottom(x));
-    // }
-
-    updateStateFromProps = props => {
-        //console.log("hello");
-        //const x = this.state.x.domain(d3.extent(props.data, d => d.date));
-        //this.setState({ x });
-    };
-
     render() {
         return <div>{this.props.chart}</div>;
     }
@@ -89,79 +90,54 @@ class Chart extends React.Component {
         // This will create a faux div and store its virtual DOM in state.chart
         var faux = this.props.connectFauxDOM("div", "chart");
 
-        // parse the date / time
-        var parseTime = d3.timeParse("%Q");
-        // var midnight = parseTime("00:00:00");
-
-        // define the area
-        var area = d3
-            .area()
-            .x(function(d) {
-                return x(d.timestamp);
-            })
-            .y0(height)
-            .y1(function(d) {
-                return y(d.cpu);
-            });
-
-        // define the line
-        var valueline = d3
-            .line()
-            .x(function(d) {
-                return x(d.timestamp);
-            })
-            .y(function(d) {
-                return y(d.cpu);
-            });
-
-        // append the svg obgect to the body of the page
-        // appends a 'group' element to 'svg'
-        // moves the 'group' element to the top left margin
-
+        // Initialize graph
         var svg = d3
             .select(faux)
             .append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
+            .attr("class", "graph")
             .attr(
                 "transform",
                 "translate(" + margin.left + "," + margin.top + ")"
             );
 
-        // format the data
-        data.forEach(function(d) {
-            d.timestamp = parseTime(d.timestamp);
-            d.cpu = +d.cpu;
-        });
-
-        x.domain(d3.extent(data, d => d.timestamp));
-
+        // Set domain range for axes
+        x.domain([10, 0]);
         y.domain([0, 100]);
 
-        // add the area
-        svg
-            .append("path")
-            .data([data])
-            .attr("class", "area")
-            .attr("d", area);
-
-        // add the valueline path.
-        svg
-            .append("path")
-            .data([data])
-            .attr("class", "line")
-            .attr("d", valueline);
+        // add a path tags for the area and line
+        svg.append("path").attr("class", "area");
+        svg.append("path").attr("class", "line");
 
         // add the X Axis
         svg
             .append("g")
             .attr("class", "x-axis")
             .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+            .call(
+                d3
+                    .axisBottom(x)
+                    .ticks(3)
+                    .tickFormat(d => d + "m ago")
+            );
 
         // add the Y Axis
         svg.append("g").call(d3.axisLeft(y));
+
+        svg
+            .selectAll("dot")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("r", 3.5)
+            .attr("cx", function(d) {
+                return x(d.timestamp);
+            })
+            .attr("cy", function(d) {
+                return y(d.cpu);
+            });
     }
 
     updateD3() {
@@ -171,9 +147,18 @@ class Chart extends React.Component {
         // reattach to faux dom
         var faux = this.props.connectFauxDOM("div", "chart");
 
-        var parseTime = d3.timeParse("%Q");
+        var svg = d3
+            .select(faux)
+            .select("svg")
+            .select(".graph");
 
-        // define the area
+        // format the data
+        data.forEach(function(d) {
+            d.timestamp = timeSince(d.timestamp);
+            d.cpu = +d.cpu;
+        });
+
+        // Define the area and line with updated data
         var area = d3
             .area()
             .x(function(d) {
@@ -193,27 +178,36 @@ class Chart extends React.Component {
                 return y(d.cpu);
             });
 
-        var svg = d3.select(faux).select("svg");
-
-        // format the data
-        data.forEach(function(d) {
-            d.timestamp = parseTime(d.timestamp);
-            d.cpu = +d.cpu;
-        });
-
-        x.domain(d3.extent(data, d => d.timestamp));
-
+        // Render line and area
         svg
             .select(".area")
-            .data([data])
+            .datum(data)
             .attr("d", area);
 
         svg
             .select(".line")
-            .data([data])
+            .datum(data)
             .attr("d", valueline);
 
-        svg.select(".x-axis").call(d3.axisBottom(x));
+        // rejoin data
+        var circle = svg.selectAll("dot").data(data);
+
+        // remove unneeded circles
+        circle.exit().remove();
+
+        // Add the scatterplot
+        // svg
+        //     .selectAll("dot")
+        //     .data(data)
+        //     .enter()
+        //     .append("circle")
+        //     .attr("r", 3.5)
+        //     .attr("cx", function(d) {
+        //         return x(d.timestamp);
+        //     })
+        //     .attr("cy", function(d) {
+        //         return y(d.cpu);
+        //     });
 
         this.props.animateFauxDOM(800);
     }
